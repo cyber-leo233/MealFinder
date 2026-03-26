d53dddc56999febf285860aa7347fad9bb102934
Nate <Googeg89@gmail.com>
2026-03-26 18:21:05 -0400

    working on js

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ single_mealEl = document.getElementById('single-meal');
 
 //Event listeners
 
+function searchMeal(e) {
+    e.preventDefault();
+    //Clear single meal
+    single_mealEl.innerHTML = '';
+    //Get search term
+  const term = search.value;
+ // check for empty
+ if(term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {  console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+        if(data.meals === null) {
+            resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+        } else {        mealsEl.innerHTML = data.meals.map(meal => `
+            <div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                <h3>${meal.strMeal}</h3>
+            </div>
+        `).join(''); }
+    });
+    
+     
+}
+else{    alert('Please enter a search term');}
+}
+
 submit.addEventListener('submit', searchMeal);
